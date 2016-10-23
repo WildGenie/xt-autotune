3eb7285662e5edae8bd7592ffe4cbab8561d758d
sjoerd <sjoerdvankreel@gmail.com>
2016-10-23 12:49:09 +0200

    Initial.

--- AutoTune/Drivers/DailyMotion/DailyMotionSearch.cs
+++ AutoTune/Drivers/DailyMotion/DailyMotionSearch.cs
@@ -1,4 +1,5 @@
-﻿using AutoTune.Shared;
+﻿using AutoTune.Settings;
+using AutoTune.Shared;
 using Newtonsoft.Json;
 using System;
 using System.Linq;
@@ -9,14 +10,20 @@ namespace AutoTune.Drivers.DailyMotion {
     class DailyMotionSearch : Search {
 
         const string Fields = "description,embed_url,id,thumbnail_120_url,title,url";
-        const string EndpointFormat = "https://api.dailymotion.com/videos/?fields={0}&search={1}";
+        const string RelatedEndpointFormat = "https://api.dailymotion.com/video/{0}/related?fields={1}&page={2}&limit={3}";
+        const string SearchEndpointFormat = "https://api.dailymotion.com/videos/?fields={0}&search={1}&page={2}&limit={3}";
 
         protected override SearchResult Execute(int totalResults, string pageToken, string query, Result similarTo) {
+            string url;
             string responseString = null;
-            using (WebClient client = new WebClient()) {
-                string url = string.Format(EndpointFormat, Fields, Uri.EscapeDataString(query));
+            var app = AppSettings.Instance;
+            int page = pageToken == null ? 1 : int.Parse(pageToken);
+            if (query != null)
+                url = string.Format(SearchEndpointFormat, Fields, Uri.EscapeDataString(query), page, app.SearchPageSize);
+            else
+                url = string.Format(RelatedEndpointFormat, similarTo.VideoId, Fields, page, app.SearchPageSize);
+            using (WebClient client = new WebClient())
                 responseString = client.DownloadString(url);
-            }
             var response = JsonConvert.DeserializeObject<DailyMotionResponse>(responseString);
             return new SearchResult {
                 State = new SearchState {
