4d7a1cc5358ed4d0ed5c27cc023e4c09e9a50b98
sjoerd <sjoerdvankreel@gmail.com>
2016-10-28 23:16:24 +0200

    Initial.

--- AutoTune/Settings/ThemeSettings.cs
+++ AutoTune/Settings/ThemeSettings.cs
@@ -7,7 +7,7 @@ namespace AutoTune.Settings {
 
         internal string BackColor1 { get; set; } = "#000000";
         internal string BackColor2 { get; set; } = "#181818";
-        internal string BackColor3 { get; set; } = "#303030";
+        internal string BackColor3 { get; set; } = "#404040";
         internal string ForeColor1 { get; set; } = "#cccccc";
         internal string ForeColor2 { get; set; } = "#00adef";
 
